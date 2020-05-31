7b6c1fb09d51946170c8521fb85c2e59a52aafb0
kashpitan <kashpitan@googlemail.com>
2020-05-31 13:10:59 +0100

    calculated average syllable per word

--- lyrics_scraper_2.py
+++ lyrics_scraper_2.py
@@ -7,8 +7,15 @@ from nltk.probability import FreqDist
 from nltk.tokenize import TweetTokenizer
 import re
 import os
+
+#genius api calls
+import requests
+
+#testing syllable count impl #1
 import hyphenate
 
+#testing syllable count impl #2
+from nltk.corpus import cmudict
 
 def get_single_song_lyrics(song_url):
 
@@ -17,7 +24,6 @@ def get_single_song_lyrics(song_url):
 
     soup = BeautifulSoup(webpage,"lxml")
     html = soup.prettify()
-    # print(html)
 
     #create two lyricsDiv variables(see try except below)
     lyricsDiv = soup.find("div", {"class": "lyrics"})
@@ -88,10 +94,65 @@ def get_average_word_length(frequency_tokenized_lyrics):
 #use frequency tokenized lyrics for now
 def get_average_syllable_count(frequency_tokenized_lyrics):
 
+    hyphenated_lyrics = []
+    for word in frequency_tokenized_lyrics:
+        hyphenated_lyrics.append(hyphenate.hyphenate_word(word))
+    # print(hyphenated_lyrics)
+
+    syllable_count_per_word = []
+    for hyphenated_word in hyphenated_lyrics:
+        syllable_count_per_word.append(len(hyphenated_word))
+        # print(str(len(hyphenated_word)) + " " + str(hyphenated_word))
+
+    # print(syllable_count_per_word)
+
+    average_syllable_count = sum(syllable_count_per_word)/len(syllable_count_per_word)
+    average_syllable_count = "{0:0.2f}".format(average_syllable_count)
+    return average_syllable_count
+
+#second function with an alternative method for calculating the average syllable count
+def get_average_syllable_count_2(frequency_tokenized_lyrics):
+    d = cmudict.dict()
+    syllable_per_word = []
+    sylllabe = None
+    for word in frequency_tokenized_lyrics:
+        try:
+            syllables = [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
+            syllable_per_word.append(syllables[0])
+            print(str(word) + " " + str(syllables[0]))
+        except(KeyError):
+            continue
+    print(syllable_per_word)
+    total_syllables = sum(syllable_per_word)
+    average_syllables = total_syllables/len(syllable_per_word)
+    average_syllables = "{0:0.2f}".format(average_syllables)
+    print(average_syllables)
+    return average_syllables
+
+#
+def get_song_info_genius():
+    search_url = "http://api.genius.com/search/"
+    access_token = "Ecr32ujznnSmv4yLPRQ_bjz7q3XLq3K-8TKOY8a2n-CXbZOVu5PzfxytGSscrSZk"
+    # headers = {'Authorization': 'Bearer Ecr32ujznnSmv4yLPRQ_bjz7q3XLq3K-8TKOY8a2n-CXbZOVu5PzfxytGSscrSZk'}
+    song_name = "diawb"
+    token = 'Bearer {}'.format(access_token)
+    data = {'q': song_name}
+    headers = {'Authorization': token}
+    response = requests.get(search_url, params=data, headers=headers)
+    jsonResponse = response.json()
+    print(jsonResponse)
+    song_info = jsonResponse["response"]["hits"][0]["result"]
+    print(song_info)
+
+# def get_song_info_lastfm():
+
 
 # test url to scrape from
 # url = "https://genius.com/Microwave-diawb-lyrics"
-url = "https://genius.com/Kendrick-lamar-maad-city-lyrics"
+# url = "https://genius.com/Kendrick-lamar-maad-city-lyrics"
+url = "https://genius.com/Eminem-godzilla-lyrics"
+# url = "https://genius.com/Aesop-rock-none-shall-pass-lyrics"
+# url = "https://genius.com/Lupe-fiasco-mural-lyrics"
 
 # for testing manually inputting a genius url
 # url = input("input a genius url here")
@@ -114,6 +175,11 @@ print("number of unique words: " + str(get_unique_words(frequency_tokenized_lyri
 average_word_length = get_average_word_length(frequency_tokenized_lyrics)
 print("average word length: " + average_word_length + " letters")
 
+#test getting average syllable count
+print("average syllables per word: " + get_average_syllable_count(frequency_tokenized_lyrics))
+# print(get_average_syllable_count_2(frequency_tokenized_lyrics))
+
+# get_song_info()
 # print(lyrics)
 # print(lyrics_words)
 # print(filtered_sentence)
